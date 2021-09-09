072792b605e1a9daac32241f3b719bd3448b7157
Carla <carlamatoss@live.com>
2021-09-08 23:28:43 -0300

    Teste API Com a Iterassys Alterando valores método alterar

--- src/test/java/meuprojeto/Pet.java
+++ src/test/java/meuprojeto/Pet.java
@@ -1,5 +1,6 @@
 package meuprojeto;
 
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 import java.nio.file.Files;
@@ -13,6 +14,7 @@ import static org.hamcrest.Matchers.contains;
 public class Pet {
 
     String uri = "https://petstore.swagger.io/v2/pet";
+    String uri_token = "";
 
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
@@ -55,6 +57,64 @@ public class Pet {
                 .body("status", is("available"))
         ;
 
+    }
+
+    /* @Test(priority = 3)
+     public void incluirToken() throws IOException {
+         String jsonBody = lerJson("Jsons/token.json");
+
+         String uri1;
+         given()
+                 .contentType("aplication/json")
+                 .log().all()
+                 .when()
+                 .get(uri1 + "/" +uri_token)
+                 .then()
+                 .log().all()
+                 .statusCode(200)
+                 .body("status", is("available"))
+                 .path("category.name");
+
+         System.out.println(" O token é:" + token);
+     }
+
+     @Test(priority = 4)
+     public void extrairToken() {
+
+
+                 given()
+                         .contentType("aplication/json")
+                         .log().all()
+                         .when()
+                         .get(uri1 + "/" + uri_token)
+                         .then()
+                         .log().all()
+                         .statusCode(200)
+                         .body("name", is("Akita"))
+                         .body("category.name", is("dog"))
+                         .body("status", is("available"))
+                         .extract()
+                         .path("category.name");
+
+
+     }
+ */
+    @Test (priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("Jsons/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Akita"))
+                .body("status", is("sold"))
+                ;
     }
 
 }
